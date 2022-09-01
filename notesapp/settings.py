88d558d151247b223c3a1f2c5715179cead12eb8
David Turchetta <davidturchetta@outlook.com>
2022-09-01 14:21:41 -0400

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -28,7 +28,6 @@ DEBUG = False
 ALLOWED_HOSTS=['david-turchetta-notebooks.herokuapp.com',
 '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -123,19 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static/'
-# ]
-STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
